440382fce9579e8f36bc1acd207cc15388f0e52c
Leoul1900 <leoulg91@gmail.com>
2019-09-29 15:49:08 +0300

    add hi bot

--- mybot.php
+++ mybot.php
@@ -1,6 +1,13 @@
 <?php
 
 
-// hellllo  old Friend
+define ('url',"https://api.telegram.org/bot519971550:AAFUFUiLlpcOsCKUq7erj47H1fFF088X_d0/");
+$update = json_decode(file_get_contents('php://input') ,true);
+$chat_id = $update['message']['chat']['id'];
+$name = $update['message']['from']['first_name'];
+
+$message = 'Hi '.$name;
+file_get_contents(url."sendmessage?text=".$received_message."&chat_id=".$chat_id.");
+
 
 ?>
